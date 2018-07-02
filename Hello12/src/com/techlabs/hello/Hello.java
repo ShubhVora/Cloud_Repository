19e594b3840fb3e4833747c9b7cc2700147b848f
unknown <ShubhVora>
2018-07-02 15:15:32 +0530

    Hello world program

---
+++ Hello12/src/com/techlabs/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.techlabs.hello;
+
+public class Hello {
+	public static void main(String args[])
+	{
+
+		int a=25;
+		System.out.println("Hello World");
+
+		System.out.println(a);
+}}
